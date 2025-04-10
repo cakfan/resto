373579954d051c962e69882f74c822f738dec6a6
cakfan <taufan.fhm8@gmail.com>
2025-04-10 14:34:07 +0700

    feat: add ads section

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 40,
+    grabCursor: true,
+    centeredSlides: false,
+  });
+});
